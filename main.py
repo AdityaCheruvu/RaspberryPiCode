9192b0eefd8e537538eea51645413082111cba7e
adityacheruvu <adityacheruvucs@gmail.com>
2019-02-21 01:53:51 +0530

    added code for SSH copy, and testing

--- main.py
+++ main.py
@@ -7,11 +7,12 @@ def copyOverSSH(src, dest):
     client = paramiko.SSHClient()
     user = "aditya"
     pswd = "aditya123*" 
-    serverIp = "192.168.0.10"    
+    serverIp = "192.168.0.10"  
+    clientIp = "192.168.0.4"  
     client.get_host_keys().add(serverIp, 'ssh-rsa', key)
     
     client.connect(serverIp, username=user, password="aditya123*")
-    stdin, stdout, stderr = client.exec_command('scp ' + src + " " + user + "@" + serverIp + ":" + dest)
+    stdin, stdout, stderr = client.exec_command('scp ' + "pi@" + clientIp + ":" + src + " " + dest)
     if(stderr!=""):
         raise Exception("CopyUnsuccessfulOverSSH")
 
