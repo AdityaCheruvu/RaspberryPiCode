e3b7b3e3d36191385f82b11896cc4379a3b29b8f
adityacheruvu <adityacheruvucs@gmail.com>
2019-02-21 01:44:51 +0530

    added code for SSH copy, and testing

--- main.py
+++ main.py
@@ -1,7 +1,23 @@
+import base64
+import paramiko
+import picamera
+
+def copyOverSSH(src, dest):
+    key = paramiko.RSAKey(data=base64.b64decode(b'AAA...'))
+    client = paramiko.SSHClient()
+    user = "aditya"
+    pswd = "aditya123*" 
+    serverIp = "192.168.0.10"    
+    client.get_host_keys().add(serverIp, 'ssh-rsa', key)
+    
+    client.connect(serverIp, username=user, password="aditya123*")
+    stdin, stdout, stderr = client.exec_command('scp ' + src + " " + user + "@" + serverIp + ":" + dest)
+    if(stderr!=""):
+        raise Exception("CopyUnsuccessfulOverSSH")
 
 def captureImg(num):
-    import picamera
     camera = picamera.PiCamera()
     camera.capture(str(num) + ".jpg")
 
-captureImg(1)
+captureImg(3)
+copyOverSSH("3.jpg", "ssh.jpg")
